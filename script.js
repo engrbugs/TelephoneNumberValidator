aa0b74e6df7e193f4521ba304d2146417d19a4ff
engrbugs <engrbugs@gmail.com>
2020-06-20 21:47:47 -0700

    done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hw');
+function telephoneCheck(str) {
+    let regex = /^(1 |1|1-)*(\(\d{3}\)|\d{3})[\s\-]?(\d{3})[\s\-]?(\d{4})$/g;
+    console.log(str, regex.test(str))
+    return regex.test(str);
+  }
+
+  
+telephoneCheck("1(555)555-5555")
+telephoneCheck("555-5555") 
+telephoneCheck("5555555") 
+telephoneCheck("1 555)555-5555") 
+
